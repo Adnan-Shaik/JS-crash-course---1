a8ffe395f947e77a9adb8f570229b8bf2fd02515
Shaik Adnan <shaikmadnan@gmail.com>
2020-12-15 18:32:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const todos = [
+  {
+   id : 1,
+   schoolwork : "Maths homework",
+   extrawork : "do house broom",
+   is_completed : true
+  },
+  {
+    id : 2,  
+    schoolwork : "English homework",
+    extrawork : "do house broom",
+    is_completed : false
+   },
+   {
+    id : 3,  
+    schoolwork : "Biology homework",
+    extrawork : "do house broom",
+    is_completed : true
+   }
+]
+for(let i = 0; i <= 10; i++){
+  console.log('for loop number : ${i}')
+}
